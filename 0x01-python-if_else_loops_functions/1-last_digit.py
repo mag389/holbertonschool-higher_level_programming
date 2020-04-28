f7a8430baac63fd25c68853b2e7459de96361189
mag389 <mag389@cornell.edu>
2020-04-28 16:29:40 +0000

    fixing spaces issue and negative issue because negative % 10 is positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of ", number, " is", end=' ')
+digit = number % 10
 if number < 0:
-    print("-", -1 * number % 10, end='')
+    digit = -1 * number % 10 * -1
+print("Last digit of", number, "is", digit, end=' ')
+if digit > 5:
+    print("and is greater than 5")
+elif digit == 0:
+    print("and is 0")
 else:
-    print(number % 10, end='')
-if number % 10 > 5:
-    print(" and is greater than 5")
-elif number % 10 == 0:
-    print(" and is 0")
-else:
-    print(" and is less than six and not zero")
+    print("and is less than 6 and not 0")
