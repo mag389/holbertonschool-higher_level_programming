ec2c001a24d193fe8da965b27e544520b621f1a0
mag389 <mag389@cornell.edu>
2020-04-28 19:16:01 +0000

    only null check for head not *head

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,7 +14,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current;
 
 	current = *head;
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
