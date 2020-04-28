de486384e92a37694a9bd99964181b38e5a3044d
mag389 <mag389@cornell.edu>
2020-04-28 18:04:42 +0000

    our own verion of upper()

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 123:
+            c = 32
+        else:
+            c = 0
+        print(chr(ord(i) - c), end='')
+    print()
