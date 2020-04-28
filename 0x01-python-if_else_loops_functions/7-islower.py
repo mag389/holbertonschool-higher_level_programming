9e4be34f7ae51998f600db2c91f581d6caa79ca4
mag389 <mag389@cornell.edu>
2020-04-28 17:41:51 +0000

    removed comments

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,4 @@
 #!/bin/usr/python3
-# islower - determines letter case
-# @c: the letter to test
-# Return: true or false
 def islower(c):
     if ord(c) > 96 and ord(c) < 124:
         return(True)
