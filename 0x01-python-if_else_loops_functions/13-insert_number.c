65c25cd7d4b358ebbc0d65fa1cf036b6f7f0ba4d
mag389 <mag389@cornell.edu>
2020-04-28 18:54:47 +0000

    mock interview question for instering into sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* insert_node - insters node at sorted location
+* Return: address of new node or null if it failed
+* @head: the head of the list
+* @number: number to use as alue for new node
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next && current->next->n < new->n)
+			current = current->next;
+		new->next = current->next;
+		current->next = new;
+	}
+	return (new);
+}
