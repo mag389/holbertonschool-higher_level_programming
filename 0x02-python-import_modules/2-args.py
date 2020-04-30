2458110861de11eb1ce69cafb372e8dbc4f7d6cd
mag389 <mag389@cornell.edu>
2020-04-30 18:00:15 +0000

    added part saying number of args and pluralized correctly

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import *
+    print("{:d} argument".format(len(argv) - 1),end='' )
     if len(argv) is 1:
-        print("0 arguments.")
+        print("s.")
+    elif len(argv) is 2:
+        print(":")
+        print(argv[1])
     else:
+        print("s:")
         for i in range(1, len(argv)):
             print("{:d}: {}".format(i, argv[i]))
