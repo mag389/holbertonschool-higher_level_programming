ec31643ae01446aec66f9bb6bebcf540899b0ba7
mag389 <mag389@cornell.edu>
2020-04-30 18:59:34 +0000

    calculator program, correct errors. pep8

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calc
+    from sys import argv
+    if len(argv) is not 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] is '+':
+        print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    elif argv[2] is "-":
+        print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    elif argv[2] is "*":
+        print("{:d} * {:d} = {:d}".format(a, b, calc.div(a, b)))
+    elif argv[2] is "/":
+        print("{:d} / {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
