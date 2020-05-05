b0391cec4e10368eb0c5bc4b5ae117ad8dbbe0f6
mag389 <mag389@cornell.edu>
2020-05-05 19:03:36 +0000

    finds max int of list, now for all negative lists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max = 0
+    max = my_list[1]
     for i in range(0, len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
