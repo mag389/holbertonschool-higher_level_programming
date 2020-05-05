14302338168f7eb2f98956c0abff06638343bba4
mag389 <mag389@cornell.edu>
2020-05-05 20:08:07 +0000

    adding null check for matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        print("")
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        print("")
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j < len(matrix) - 1:
-                print(" ", end="")
-            else:
-                print("")
+    if matrix:
+        if len(matrix) == 0:
+            print("")
+        if len(matrix) == 1 and len(matrix[0]) == 0:
+            print("")
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j < len(matrix) - 1:
+                    print(" ", end="")
+                else:
+                    print("")
