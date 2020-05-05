7ba1d614ee327e2d12fe3e3ea19b0a2c0fc64f46
mag389 <mag389@cornell.edu>
2020-05-05 20:44:34 +0000

    fixed index

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list:
         if len(my_list) == 0:
             return None
-        max = my_list[1]
+        max = my_list[0]
         if my_list:
             for i in my_list:
                 if i > max:
