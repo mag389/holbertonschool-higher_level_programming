abd1f70a5c96eb21013cb1c3df46e99cdde123c7
mag389 <mag389@cornell.edu>
2020-05-05 20:38:24 +0000

    adding null check and return None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    max = my_list[1]
     if my_list:
-        for i in my_list:
-            if i > max:
-                max = i
-    return max
+        if len(my_list) == 0:
+            return None
+        max = my_list[1]
+        if my_list:
+            for i in my_list:
+                if i > max:
+                    max = i
+        return max
+    return None
