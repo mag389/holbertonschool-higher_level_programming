cf1fbfa1445c26f6da22935ea7142586c01b409b
mag389 <mag389@cornell.edu>
2020-05-06 14:55:36 +0000

    using translate instead

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newstring = ""
-    for i in range(0, len(my_string)):
-        if my_string[i] is not 'c' and my_string[i] is not 'C':
-            newstring += my_string[i]
-    return newstring
+    return my_string.translate({ord(i): None for i in 'cC'})
