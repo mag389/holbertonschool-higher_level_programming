a40793139a1a6b32fcea871945337bd42e388b1e
mag389 <mag389@cornell.edu>
2020-05-07 19:50:38 +0000

    fixed zero check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
-        return None
+        return 0
     nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(0, len(roman_string) - 1):
