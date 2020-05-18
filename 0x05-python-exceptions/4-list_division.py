354009855a34b7494241ca50bf2a1be72c900d02
mag389 <mag389@cornell.edu>
2020-05-18 16:32:19 +0000

    adding finally part

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,4 +10,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         except ZeroDivisionError:
             print("division by 0")
+        finally:
+            pass
     return new_list
