cefef3aaab5b38c10c25c89bcd17800080aa1418
mag389 <mag389@cornell.edu>
2020-05-18 15:07:56 +0000

    prints list of ints, right now i made it too safe, doesn't match the output

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j = j + 1
+        except TypeError:
+            break
+        except ValueError:
+            pass
+    print("")
+    return j
