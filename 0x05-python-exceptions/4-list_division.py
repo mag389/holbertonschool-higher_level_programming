aaee30c2fa92701efc0f3f9e6e8c867e9188d997
mag389 <mag389@cornell.edu>
2020-05-18 15:29:51 +0000

    divides list elements

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [0 for i in range(0, list_length)]
+    for i in range(0, list_length):
+        try:
+            new_list[i] = my_list_1[i] / my_list_2[i]
+        except (TypeError, ValueError):
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+    return new_list
