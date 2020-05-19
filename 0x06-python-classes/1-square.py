780adc5b7245cd6116cb002a497deff36d0a74e5
mag389 <mag389@cornell.edu>
2020-05-19 17:10:38 +0000

    square class with private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+class Square:
+    """
+    Square - the square class
+
+    Currently blank class
+    __init__ - makes instance
+    """
+    def __init__(self, _Square_size=0):
+        """
+        creates square instance
+
+        _Square_size: self explanatory
+        """
+        self._Square__size = _Square_size
+        pass
