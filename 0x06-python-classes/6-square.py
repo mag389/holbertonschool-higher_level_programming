a789d62b7a2a2e432984907526e025a5012f3030
mag389 <mag389@cornell.edu>
2020-05-19 20:50:16 +0000

    checking for elements of position are ints and fixing syntax

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,8 +17,8 @@ class Square:
         """
         if type(position) is not tuple or len(position) is not 2:
             raise(TypeError("position must be a tuple of 2 positive integers"))
-	if type(position[0]) is not int or type(position[1]) is not int:
-            raise(TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise(TypeError("position must be a tuple of 2 positive integers"))
         if position[1] < 0 or position[0] < 0:
             raise(TypeError("position must be a tuple of 2 positive integers"))
         if type(size) is not int:
@@ -37,6 +37,8 @@ class Square:
     def position(self, value):
         """setter for position"""
         if type(value) is not tuple or len(value) is not 2:
+            raise(TypeError("position must be a tuple of 2 positive integers"))
+        if type(position[0]) is not int or type(position[1]) is not int:
             raise(TypeError("position must be a tuple of 2 positive integers"))
         if value[1] < 0 or value[0] < 0:
             raise(TypeError("position must be a tuple of 2 positive integers"))
