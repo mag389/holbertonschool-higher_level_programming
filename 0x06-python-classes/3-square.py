014e56fa79446be63971b3d4bb31ede3bf6a3b3b
mag389 <mag389@cornell.edu>
2020-05-19 20:16:22 +0000

    file docstring

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""area method file"""
+
+
+class Square:
+    """
+    Square - the square class
+
+    Currently blank class
+    __init__ - makes instance
+    """
+    def __init__(self, _Square_size=0):
+        """
+        creates square instance
+
+        _Square_size: self explanatory
+        """
+        if type(_Square_size) is not int:
+            raise(TypeError("size must be an integer"))
+        if _Square_size < 0:
+            raise(ValueError("size must be >= 0"))
+        self._Square__size = _Square_size
+
+    def area(self):
+        return self._Square__size ** 2
