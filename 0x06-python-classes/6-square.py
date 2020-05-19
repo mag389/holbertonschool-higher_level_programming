35630efb0ffbcbef6cc4395681642418ad1eb2fa
mag389 <mag389@cornell.edu>
2020-05-19 20:45:30 +0000

    making sure position is ints

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,6 +17,8 @@ class Square:
         """
         if type(position) is not tuple or len(position) is not 2:
             raise(TypeError("position must be a tuple of 2 positive integers"))
+	if type(position[0]) is not int or type(position[1]) is not int:
+            raise(TypeError("position must be a tuple of 2 positive integers")
         if position[1] < 0 or position[0] < 0:
             raise(TypeError("position must be a tuple of 2 positive integers"))
         if type(size) is not int:
