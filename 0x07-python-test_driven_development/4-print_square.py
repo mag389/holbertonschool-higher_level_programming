2eea689e735da71297e395a6a42a03830ce055b0
mag389 <mag389@cornell.edu>
2020-05-21 18:41:28 +0000

    print square method and test cases

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""print square func for testing"""
+
+
+def print_square(size):
+    """print square function """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        for j in range(0, size):
+            print("#", end="")
+        print("")
+    if size is 0:
+        print("")
