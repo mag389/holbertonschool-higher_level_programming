c8b740f9b18a5e6f3f87587b390ddc0c4204deda
mag389 <mag389@cornell.edu>
2020-05-24 19:45:37 +0000

    made the area comparison correctly static, added class method for square constructor

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -80,3 +80,9 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """ returns new square """
+        square = cls(size, size)
+        return square
