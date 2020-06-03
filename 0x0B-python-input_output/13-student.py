99bf2103855d73c02e4ae0818130a41aeeb33712
mag389 <mag389@cornell.edu>
2020-06-03 19:40:42 +0000

    replaces old dict object with the stored one

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" the student class file """
+
+
+class Student:
+    """ the student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ instantiation with name and age """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns dict definition of a student"""
+        if attrs is None:
+            return self.__dict__
+        if type(attrs) is not list:
+            return self.__dict__
+        for elem in attrs:
+            if type(elem) is not str:
+                return self.__dict__
+        newdict = {}
+        for elem in attrs:
+            try:
+                newdict[elem] = self.__dict__[elem]
+            except:
+                pass
+        return newdict
+
+    def reload_from_json(self, json):
+        """ reloads object from json"""
+        self.__dict__ = json
