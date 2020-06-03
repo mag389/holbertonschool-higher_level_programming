35c18a2eeb19d32a7fe67e1e589afe2113db814f
mag389 <mag389@cornell.edu>
2020-06-03 19:16:05 +0000

    now returns only certain dict elements of an object

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" the student class file """
+
+
+class Student:
+    """ the student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ instantiation with name and age """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns dict definition of a student"""
+        if attrs is None:
+            return self.__dict__
+        if type(attrs) is not list:
+            return self.__dict__
+        for elem in attrs:
+            if type(elem) is not str:
+                return self.__dict__
+        newdict = {}
+        for elem in attrs:
+            try:
+                newdict[elem] = self.__dict__[elem]
+            except:
+                pass
+        return newdict
