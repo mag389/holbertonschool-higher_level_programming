0008cb17642cbc0e8a5ccb9d7d9f651a082c9197
mag389 <mag389@cornell.edu>
2020-06-04 19:01:10 +0000

    correct error message with if statement

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,4 +4,6 @@
 
 def add_attribute(self, name, value):
     """ sets attribute"""
+    if hasattr(self, "__dict__") is False:
+        raise TypeError("can't add new attribute")
     self.__setattr__(name, value)
