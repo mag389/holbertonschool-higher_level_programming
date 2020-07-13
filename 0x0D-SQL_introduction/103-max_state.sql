8f6b16bb6d721dee8abc3e01611ea0fdac0ea615
mag389 <mag389@cornell.edu>
2020-07-13 19:11:08 +0000

    explicitly order by state name

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,6 @@
 -- max temps by state
 -- selects state and max temp from that state grouped by state
-select state, MAX(value) AS max_temp FROM temperatures GROUP BY state;
+SELECT state, MAX(value) AS max_temp 
+FROM temperatures 
+GROUP BY state
+ORDER BY state ASC;
