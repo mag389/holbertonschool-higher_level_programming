c1e7ef7510d4aeaf0b9683e217f068a0ea1f17ad
mag389 <mag389@cornell.edu>
2020-07-14 19:46:21 +0000

    join with count

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- count shows by genre
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC;
