d5cb1117c910c51c2079652eed4b7de234bd7cbb
mag389 <mag389@cornell.edu>
2020-08-09 20:40:34 +0000

    and the models tstae file

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" onto the alchemy section """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ the state class """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __str__(self):
+        """ str attr """
+        return "{}".format(self.name)
