171368be62a70d155bb48ebc99d8c4358b150861
mag389 <mag389@cornell.edu>
2020-08-09 19:16:25 +0000

    some injection protection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" states script """
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    state = sys.argv[4]
+    if len(state) > 15:
+        exit()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+    ORDER BY id ASC".format(state))
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
