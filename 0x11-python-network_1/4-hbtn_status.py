711960555d1d7bb7924d0067eca41c3136e118a2
mag389 <mag389@cornell.edu>
2020-08-24 18:27:22 +0000

    type of content, not response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("	- type: {}".format(type(r)))
+    print("	- type: {}".format(type(r.text)))
     print("	- content: {}".format(r.text))
