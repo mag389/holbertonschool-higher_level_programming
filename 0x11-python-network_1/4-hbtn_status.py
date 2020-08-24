979bfc08bb625a588aa6d03de3f44b0ff0a0d2c4
mag389 <mag389@cornell.edu>
2020-08-24 18:25:59 +0000

    formatting of prints

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print(type(r))
-    print(r.text)
+    print("	- type: {}".format(type(r)))
+    print("	- content: {}".format(r.text))
