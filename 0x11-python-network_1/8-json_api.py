6b34d1918a2809a479c219e223691d85ee664b10
mag389 <mag389@cornell.edu>
2020-08-24 19:25:32 +0000

    json testing

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" passing a letter and testing json """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = "http://0.0.0.0:5000/search_user"
+    if (len(sys.argv) > 1):
+        q = sys.argv[1]
+    else:
+        q = ""
+    data = {"q": q}
+    r = requests.post(url, data)
+    try:
+        json = r.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("{} {}".format(json.get("id"), json.get("name")))
+    except:
+        print("Not a valid JSON")
