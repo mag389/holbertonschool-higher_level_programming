6711b75da40920afb495655968ff9cb0766303dd
mag389 <mag389@cornell.edu>
2020-08-24 18:23:53 +0000

    requests library to do the same stuff

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetching with requests lib """
+import requests
+
+
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print(type(r))
+    print(r.text)
