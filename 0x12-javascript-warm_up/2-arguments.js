16800fda5196b1d3f40a5149ac1a9b5de3356681
mag389 <mag389@cornell.edu>
2020-09-15 18:49:26 +0000

    removing all comments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-// const process = require('process');
-// var args = process.argv;
-// console.log(args.length)
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
