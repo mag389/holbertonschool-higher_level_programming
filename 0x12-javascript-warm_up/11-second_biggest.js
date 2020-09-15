7a07411a19f07032d1088588b27665c55e3db486
mag389 <mag389@cornell.edu>
2020-09-15 20:05:47 +0000

    parseint to everything

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-if (process.argv.length <= 2) {
+if (process.argv.length <= 3) {
   console.log(0);
-} else if (isNaN(num1)) {
+/* } else if (isNaN(num1)) {
   console.log(0);
 } else if (isNaN(num2)) {
   console.log(0);
+*/
 } else {
   for (let i = 3; i < process.argv.length; i++) {
-    if (process.argv[i] > num1) {
+    if (parseInt(process.argv[i]) > num1) {
       num2 = num1;
-      num1 = process.argv[i];
-    } else if (process.argv[i] > num2) {
-      num2 = process.argv[i];
+      num1 = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) > num2) {
+      num2 = parseInt(process.argv[i]);
     }
   }
   console.log(num2);
