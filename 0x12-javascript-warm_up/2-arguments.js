9b889804ec19b0fc80d65ee0c411a4de51a2bb4a
mag389 <mag389@cornell.edu>
2020-09-15 18:41:02 +0000

    removed the const as well

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* prints based on number of given arguments to script */
-const process = require('process');
+// const process = require('process');
 // that line is not strictly neccesary
 // var args = process.argv;
 // console.log(args.length)
