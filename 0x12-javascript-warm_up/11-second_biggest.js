0b732dcf78bcbedc3b7bd55f8bab068a533f90d2
mag389 <mag389@cornell.edu>
2020-09-15 19:59:57 +0000

    adding length check explicitly

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-if (isNaN(num1)) {
+if (process.argv.length <= 2) {
+  console.log(0);
+} else if (isNaN(num1)) {
   console.log(0);
 } else if (isNaN(num2)) {
   console.log(0);
