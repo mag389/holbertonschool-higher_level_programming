ebc76e533320917cf8d8d1eff21fecfc80951a9e
mag389 <mag389@cornell.edu>
2020-09-22 20:04:24 +0000

    how many movies is wedge in using only a call to .../films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,24 @@
 #!/usr/bin/node
 const req = require('request');
-// let theUrl = 'https://swapi-api.hbtn.io/api/films/';
-const personurl = 'https://swapi-api.hbtn.io/api/people/18';
+// const RegExp = require('regex');
+const theUrl = process.argv[2];
+// const personurl = 'https://swapi-api.hbtn.io/api/people/18';
 
 // theUrl = theUrl + process.argv[2];
-req.get({ url: personurl }, function (err, res, body) {
+req.get({ url: theUrl }, function (err, res, body) {
   if (err) {
     console.log('code: ' + res.statusCode);
   } else {
     const json = JSON.parse(body);
-    console.log(json.films.length);
+    let numfilms = 0;
+    for (const film of json.results) {
+      for (const character of film.characters) {
+        // console.log(character);
+        if (character.includes('18')) {
+          numfilms += 1;
+        }
+      }
+    }
+    console.log(numfilms);
   }
 });
